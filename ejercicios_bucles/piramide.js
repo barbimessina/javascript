96feeebc1d27c7a77e7b34150a9b7fd52c8ede6b
Barbara Messina <barbimessina@hotmail.com>
2023-08-07 15:50:57 -0300

    bucles

---
+++ ejercicios_bucles/piramide.js
@@ -0,0 +1,14 @@
+//En la consola del navegador utilizando Javascript, dibujar una piramide utilizando uno de los 3 ciclos vistos en clase.
+
+let niveles = 10;
+
+
+for (let altura =1; altura <= niveles; altura++){ //altura
+    for (let espacio = 1; espacio<= niveles- altura; espacio++) {  //ancho espacio
+         document.write("&nbsp");
+    }
+    for(let ast = 1; ast <= altura; ast++){ //Asteriscos
+        document.write("*")
+    }
+document.write("<br/>")
+}
