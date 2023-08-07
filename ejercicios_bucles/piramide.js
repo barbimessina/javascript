841e9572b97d98beede17018877c0da7c83c8fff
Barbara Messina <barbimessina@hotmail.com>
2023-08-07 15:59:29 -0300

    cambios

--- ejercicios_bucles/piramide.js
+++ ejercicios_bucles/piramide.js
@@ -5,12 +5,12 @@ let niveles = 10;
 
 for (let altura =1; altura <= niveles; altura++){ //altura
     for (let espacio = 1; espacio<= niveles- altura; espacio++) {  //ancho espacio
-         console.log("&nbsp");
+         document.write("&nbsp");
     }
     for(let ast = 1; ast <= altura; ast++){ //Asteriscos
-        console.log("*")
+        document.write("*")
     }
-console.log("<br/>")
+document.write("<br/>")
 
 
 }
